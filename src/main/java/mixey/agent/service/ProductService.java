44de8f03204f487bb8d4236a31466e28b9c41c9e
mixey13 <mixey13@gmail.com>
2017-02-26 16:10:05 +0300

    ref ProductService

--- src/main/java/mixey/agent/service/ProductService.java
+++ src/main/java/mixey/agent/service/ProductService.java
@@ -1,12 +1,34 @@
 package mixey.agent.service;
 
 import mixey.agent.model.Product;
+import mixey.agent.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
-public interface ProductService {
-    Product save(Product product);
-    Product update(Product product);
-    boolean delete(Integer id);
-    Product get(Integer id);
-    List<Product> getAll();
+@Service
+public class ProductService{
+    @Autowired
+    private ProductRepository repository;
+
+    public Product save(Product product) {
+        return repository.save(product);
+    }
+
+    public Product update(Product product) {
+        return repository.save(product);
+    }
+
+    public boolean delete(Integer id) {
+        return repository.delete(id);
+    }
+
+    public Product get(Integer id) {
+        return repository.get(id);
+    }
+
+    public List<Product> getAll() {
+        return repository.getAll();
+    }
 }
